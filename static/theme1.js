4b53b76cc26ef590e227bf14b235f6fdb94c96df
MeltIce2024 <114905314+MeltIce2024@users.noreply.github.com>
2025-04-19 22:27:39 +0800

    Update theme1.js

--- static/theme1.js
+++ static/theme1.js
@@ -8,15 +8,106 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log('应用主页主题');
         let style = document.createElement("style");
         style.innerHTML = `
-        @fontface {font-family: 'JetBrains Mono'; src: url('https://meltice2024.github.io/fonts/JetBrainsMono-Regular.woff2') format('woff2'); font-weight: regular;}
-        @fontface {font-family: 'JetBrains Mono'; src: url('https://meltice2024.github.io/fonts/JetBrainsMono-Italic.woff2') format('woff2'); font-weight: regular; font-style: italic;}
-        @fontface {font-family: 'JetBrains Mono'; src: url('https://meltice2024.github.io/fonts/JetBrainsMono-Medium.woff2') format('woff2'); font-weight: medium;}
-        @fontface {font-family: 'JetBrains Mono'; src: url('https://meltice2024.github.io/fonts/JetBrainsMono-MediumItalic.woff2') format('woff2'); font-weight: medium; font-style: italic;}
-        @fontface {font-family: 'JetBrains Mono'; src: url('https://meltice2024.github.io/fonts/JetBrainsMono-Bold.woff2') format('woff2'); font-weight: bold;}
-        @fontface {font-family: 'JetBrains Mono'; src: url('https://meltice2024.github.io/fonts/JetBrainsMono-BoldItalic.woff2') format('woff2'); font-weight: bold; font-style: italic;}
-        body {font-family: 'JetBrains Mono', sans-serif;}
+        /* header布局*/
+        .blogTitle {
+            display: unset;
+        }
+        
+        #header {
+            height: 340px;
+        }
+        
+        #header h1 {
+            position: absolute;
+            left: 50%;
+            transform: translateX(-50%);
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        
+        .title-right {
+            margin: unset;
+            margin-top: 295px;
+            margin-left: 50%;
+            transform: translateX(-50%);
+        }
+        
+        .avatar {
+            width: 125px;
+            height: 125px;
+        }
+        
+        #header h1 a {
+            margin-top: 30px;
+            font-family: fantasy;
+            margin-left: unset;
+        }
+        
+        .SideNav {
+            border-radius: 10px; /* 圆角边框 */
+            min-width: unset;
+        }
+        
+        .SideNav-item:hover {
+            background-color: #DDDDDD;
+            border-radius: 10px;
+            transform: scale(1.02);
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+        }
+        
+        .SideNav-item {
+            transition: 0.5s;
+        }
+        
+        /* 分页条 */
+        .pagination a:hover, .pagination a:focus, .pagination span:hover, .pagination span:focus, .pagination em:hover, .pagination em:focus {
+            border-color: rebeccapurple;
+        }
+        
+        /* 右上角按钮 */
+        div.title-right .btn {
+            display: inline-flex;
+            align-items: center;
+            width: auto;
+            height: 40px;
+            margin: 0 3px;
+            border-radius: 2em !important;
+            transition: 0.3s;
+        }
+        
+        div.title-right .btn:hover {
+            width: auto;
+            border-radius: 2em !important;
+            background-color: #DDDDDD;
+        }
+        
+        div.title-right .btn .btndescription {
+            display: none;
+            margin-left: 3px;
+            white-space: nowrap;
+            color: black;
+            font-weight: bold;
+        }
+        
+        div.title-right .btn:hover .btndescription {
+            display: inline;
+        }
         `;
         document.head.appendChild(style);
+
+        //右上角按钮描述
+        let topright_buttons = document.querySelectorAll(".title-right a.btn");
+
+        topright_buttons.forEach(button => {
+            var title = button.getAttribute('title');
+            if (title) {
+                var btndescription = document.createElement('span');
+                btndescription.className = 'btndescription';
+                btndescription.textContent = title;
+                button.appendChild(btndescription);
+            }
+        });
 
         let footer = document.getElementById('footer');
         let counter = document.createElement('div');
@@ -32,15 +123,68 @@ document.addEventListener('DOMContentLoaded', function() {
 
         let style = document.createElement("style");
         style.innerHTML = `
-        @fontface {font-family: 'JetBrains Mono'; src: url('https://meltice2024.github.io/fonts/JetBrainsMono-Regular.woff2') format('woff2'); font-weight: regular;}
-        @fontface {font-family: 'JetBrains Mono'; src: url('https://meltice2024.github.io/fonts/JetBrainsMono-Italic.woff2') format('woff2'); font-weight: regular; font-style: italic;}
-        @fontface {font-family: 'JetBrains Mono'; src: url('https://meltice2024.github.io/fonts/JetBrainsMono-Medium.woff2') format('woff2'); font-weight: medium;}
-        @fontface {font-family: 'JetBrains Mono'; src: url('https://meltice2024.github.io/fonts/JetBrainsMono-MediumItalic.woff2') format('woff2'); font-weight: medium; font-style: italic;}
-        @fontface {font-family: 'JetBrains Mono'; src: url('https://meltice2024.github.io/fonts/JetBrainsMono-Bold.woff2') format('woff2'); font-weight: bold;}
-        @fontface {font-family: 'JetBrains Mono'; src: url('https://meltice2024.github.io/fonts/JetBrainsMono-BoldItalic.woff2') format('woff2'); font-weight: bold; font-style: italic;}
-        body {font-family: 'JetBrains Mono', sans-serif;}
+        /* markdown内容 */
+        .markdown-body img {
+            border-radius: 10px;
+        }
+
+        .markdown-alert {
+            border-radius: 10px;
+        }
+
+        .markdown-body .highlight pre, .markdown-body pre {
+            background: rgba(255, 255, 255, 0.85);
+            border-radius: 10px;
+        }
+
+        .markdown-body code, .markdown-body tt {
+            border-radius: 10px;
+        }
+
+        video {
+            border-radius: 10px;
+        }
+
+        /* 右上角按钮 */
+        div.title-right .btn {
+            display: inline-flex;
+            align-items: center;
+            width: auto;
+            height: 40px;
+            margin: 0 3px;
+            border-radius: 2em !important;
+            transition: 0.3s;
+        }
+
+        div.title-right .btn:hover {
+            width: auto;
+            border-radius: 2em !important;
+            background-color: #DDDDDD;
+        }
+
+        div.title-right .btn .btndescription {
+            display: none;
+            margin-left: 3px;
+            white-space: nowrap;
+            color: black;
+            font-weight: bold;
+        }
+
+        div.title-right .btn:hover .btndescription {
+            display: inline;
+        }
         `;
         document.head.appendChild(style);
+
+        topright_buttons.forEach(button => {
+            var title = button.getAttribute('title');
+            if (title) {
+                var btndescription = document.createElement('span');
+                btndescription.className = 'btndescription';
+                btndescription.textContent = title;
+                button.appendChild(btndescription);
+            }
+        });
     } 
 
 
@@ -50,16 +194,90 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log('应用搜索页主题');
         let style = document.createElement("style");
         style.innerHTML = `
-        @fontface {font-family: 'JetBrains Mono'; src: url('https://meltice2024.github.io/fonts/JetBrainsMono-Regular.woff2') format('woff2'); font-weight: regular;}
-        @fontface {font-family: 'JetBrains Mono'; src: url('https://meltice2024.github.io/fonts/JetBrainsMono-Italic.woff2') format('woff2'); font-weight: regular; font-style: italic;}
-        @fontface {font-family: 'JetBrains Mono'; src: url('https://meltice2024.github.io/fonts/JetBrainsMono-Medium.woff2') format('woff2'); font-weight: medium;}
-        @fontface {font-family: 'JetBrains Mono'; src: url('https://meltice2024.github.io/fonts/JetBrainsMono-MediumItalic.woff2') format('woff2'); font-weight: medium; font-style: italic;}
-        @fontface {font-family: 'JetBrains Mono'; src: url('https://meltice2024.github.io/fonts/JetBrainsMono-Bold.woff2') format('woff2'); font-weight: bold;}
-        @fontface {font-family: 'JetBrains Mono'; src: url('https://meltice2024.github.io/fonts/JetBrainsMono-BoldItalic.woff2') format('woff2'); font-weight: bold; font-style: italic;}
-        body {font-family: 'JetBrains Mono', sans-serif;}
+        /* header布局*/
+        
+        .title-right {
+            align-items: flex-end;
+        }
+        
+        .SideNav {
+            border-radius: 10px; /* 圆角边框 */
+            min-width: unset;
+        }
+        
+        .SideNav-item:hover {
+            background-color: #DDDDDD;
+            border-radius: 10px;
+            transform: scale(1.02);
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+        }
+        
+        .SideNav-item {
+            transition: 0.5s;
+        }
+        
+        
+        /* 右上角按钮 */
+        div.title-right .btn {
+            display: inline-flex;
+            align-items: center;
+            width: auto;
+            height: 40px;
+            margin: 0 3px;
+            border-radius: 2em !important;
+            transition: 0.3s;
+        }
+        
+        div.title-right .btn:hover {
+            width: auto;
+            border-radius: 2em !important;
+            background-color: #DDDDDD;
+        }
+        
+        div.title-right .btn .btndescription {
+            display: none;
+            margin-left: 3px;
+            white-space: nowrap;
+            color: black;
+            font-weight: bold;
+        }
+        
+        div.title-right .btn:hover .btndescription {
+            display: inline;
+        }
+        
+        .subnav-search-input {
+            border-radius: 2em;
+            float: unset !important;
+        }
+        
+        .subnav-search-icon {
+            top: 9px;
+        }
+        
+        button.btn.float-left {
+            display: none;
+        }
+        
+        .subnav-search {
+            width: unset; 
+            height: 36px;
+        }
         `;
         document.head.appendChild(style);
         
+        //右上角按钮描述
+        let topright_buttons = document.querySelectorAll(".title-right a.btn");
+
+        topright_buttons.forEach(button => {
+            var title = button.getAttribute('title');
+            if (title) {
+                var btndescription = document.createElement('span');
+                btndescription.className = 'btndescription';
+                btndescription.textContent = title;
+                button.appendChild(btndescription);
+            }
+        });
     
         // 搜索框回车触发
         let input = document.getElementsByClassName("form-control subnav-search-input float-left")[0];
